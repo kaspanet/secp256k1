087f4bba7890c7a6ca79432b880c0ed87e2527d3
Elichai Turkel <elichai.turkel@gmail.com>
2020-02-19 13:17:06 +0200

    Fix missing MIT license attribute in oldschnorr impl https://github.com/bitcoin-core/secp256k1/pull/425

--- src/modules/oldschnorr/oldschnorr.h
+++ src/modules/oldschnorr/oldschnorr.h
@@ -1,4 +1,5 @@
 /***********************************************************************
+ * Copyright (c) 2014-2015 Pieter Wuille                               *
  * Copyright (c) 2017 Amaury SÉCHET                                    *
  * Distributed under the MIT software license, see the accompanying    *
  * file COPYING or http://www.opensource.org/licenses/mit-license.php. *
